ce7dc6b818b0b3f3e8187b48dc07d02d8f292c10
Jade Braz Rodrigues <jadebraz14@gmail.com>
2022-12-15 17:05:33 -0300

    joias

---
+++ script.js
@@ -0,0 +1,23 @@
+$('.card').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTimeOut:2000,
+    autoplayHaverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        500:{
+            items:2,
+            nav:false,
+        },
+        1440:{
+            items:3,
+            nav:false
+        }
+    }
+})
+$('.btn').click(function(){
+    $('.menu-mobile').slideToggle('show')
+})
